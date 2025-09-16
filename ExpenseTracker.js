618b32ca9ee31bee5b1f1d48423c176b6aff39ab
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2025-09-16 12:04:41 +0300

    wrote functions

--- ExpenseTracker.js
+++ ExpenseTracker.js
@@ -1,1 +1,73 @@
-console.log("This is Expense Tracker file project");
+/*
+* @author HamidReza Ghavami
+* @date 2025-09-15
+* @version 1.0
+* @license MIT
+* @description A simple Expense Tracker CLI.
+*/
+
+// import nessary headers
+import fs from 'fs';
+import readline from 'readline-sync';
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+// create a class for the expense tracker
+class ExpenseTracker { 
+    constructor() { 
+        this.expenses = [];
+        this.totalExpenses = 0;
+    }
+
+    add() { 
+        const amount = parseFloat(readline.question("Enter the amount of money comes to your account: "));
+        const description = readline.question("Enter the description of the expense: ");
+        if ( amount > 0 ) { 
+            this.expenses.push({ amount, description});
+            this.totalExpenses += amount;
+        } else { 
+            console.log("Amount must be greater than 0!");
+        }
+    }
+
+    update() { 
+        const index = parseInt(readline.question("Enter the index of the expense you want to update: "));
+        if ( index >= 0 && index < this.expenses.length ) { 
+            const amount = parseFloat(readline.question("Enter the new amount of the expense: "));
+            const description = readline.question("Enter the new description of the expense: ");
+            this.expenses[index] = { amount, description };
+        } else { 
+            console.log("Invalid index!");
+        }
+    }
+
+    delete() { 
+        const index = parseInt(readline.question("Enter the index of the expense you want to delete: "));
+        if ( index >= 0 && index < this.expenses.length ) { 
+            this.expenses.splice(index, 1);
+        } else { 
+            console.log("Invalid index!");
+        }
+    }
+
+    view() { 
+        console.log("Expenses: ");
+        for ( let expenses of this.expenses ) { 
+            console.log(`Amount: ${expenses.amount}, description: ${expenses.description}`);
+        }
+        console.log(`Total Expenses: ${this.totalExpenses}`);
+    }
+
+    summary() { 
+        console.log("Please enter the month that for exact month's summary");
+        const month = readline.question("Enter the month: ");
+        let totalExpenses = 0;
+        for ( let expenses of this.expenses ) { 
+            if ( expenses.description.includes(month)) { 
+                totalExpenses += expenses.amount;
+            }
+        }
+        console.log(`Total Expenses in ${month}: ${totalExpenses}`);
+    }
+
+    
+}

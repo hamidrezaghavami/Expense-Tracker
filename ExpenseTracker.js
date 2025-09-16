4fd977d4faee564207d3dcb64ed023958ea749ec
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2025-09-16 16:31:02 +0300

    Second attempt for changes

--- ExpenseTracker.js
+++ ExpenseTracker.js
@@ -7,13 +7,12 @@
 */
 
 // import nessary headers
-import fs from 'fs';
 import readline from 'readline-sync';
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 // create a class for the expense tracker
 class ExpenseTracker { 
-    constructor() { 
+    constructor() {
         this.expenses = [];
         this.totalExpenses = 0;
     }
@@ -29,7 +28,7 @@ class ExpenseTracker {
         }
     }
 
-    update() { 
+    update() {
         const index = parseInt(readline.question("Enter the index of the expense you want to update: "));
         if ( index >= 0 && index < this.expenses.length ) { 
             const amount = parseFloat(readline.question("Enter the new amount of the expense: "));
@@ -58,7 +57,7 @@ class ExpenseTracker {
     }
 
     summary() { 
-        console.log("Please enter the month that for exact month's summary");
+        console.log("Please enter the month that for month's summary");
         const month = readline.question("Enter the month: ");
         let totalExpenses = 0;
         for ( let expenses of this.expenses ) { 
@@ -68,6 +67,49 @@ class ExpenseTracker {
         }
         console.log(`Total Expenses in ${month}: ${totalExpenses}`);
     }
-
-    
 }
+
+// create a main function to run the porgram
+function main () { 
+    const expenseTracker = new ExpenseTracker();
+    let input;
+    do { 
+        console.log("1. Add");
+        console.log("2. Update");
+        console.log("3. Delete");
+        console.log("4. View");
+        console.log("5. Summary");
+        console.log("6. Exit");
+        input = readline.question("Enter your choice: ");
+    switch (input) { 
+        case '1':
+            expenseTracker.add();
+            break;
+            
+        case '2':
+            expenseTracker.update();
+        break;
+
+        case '3':
+            expenseTracker.delete();
+        break;
+
+        case '4':
+            expenseTracker.view();
+        break;
+
+        case '5':
+            expenseTracker.summary();
+        break;
+
+        case '6':
+            console.log("Exiting from program!");
+        break;
+        default:
+            console.log("Invalid chooice!");
+        break;    
+        }
+    } while ( input !== '6' );
+}
+
+main();
